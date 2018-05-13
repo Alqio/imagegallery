bce5db47fafa1259c11f2f07e3bbbbf6a75344da
Kyösti Alkio <kyosti.alkio@gmail.com>
2018-05-13 21:16:50 +0300

    added dj_database_url

--- imagegallery/settings.py
+++ imagegallery/settings.py
@@ -133,4 +133,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 
