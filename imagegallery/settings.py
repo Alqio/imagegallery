10de96e292db8cdd5a47bc0b7ec2da2ca622b9e8
alqio <kyosti.alkio@gmail.com>
2022-12-19 20:15:51 +0200

    Small fixes

--- imagegallery/settings.py
+++ imagegallery/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': 'db',
-        'PORT': '6543'
+        'PORT': '5432'
     }
 }
 
